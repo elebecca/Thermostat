19c08353fbaf2eaec99d2eaaeb96bcb74e98266e
Student <student@Makerss-MacBook-Air.local>
2020-02-19 16:19:45 +0000

    removed typo

--- src/thermostat.js
+++ src/thermostat.js
@@ -25,7 +25,7 @@ Thermostat.prototype.reset = function(){
   this.temperature = 20 ;
 }
 
-Thermostat.prototype.energyUsagen = function() {
+Thermostat.prototype.energyUsage = function() {
   if (this.temperature < 18) {return 'low-usage'}
   else if (this.temperature < 25) {return 'medium-usage'}
   else {return 'high-usage'}
