6a18c2bc76ce32eda94b3b495bf67e4a88805cba
Student <student@Makerss-MacBook-Air.local>
2020-02-19 16:17:17 +0000

    adding  html

--- src/thermostat.js
+++ src/thermostat.js
@@ -25,7 +25,7 @@ Thermostat.prototype.reset = function(){
   this.temperature = 20 ;
 }
 
-Thermostat.prototype.energyUsage = function() {
+Thermostat.prototype.energyUsagen = function() {
   if (this.temperature < 18) {return 'low-usage'}
   else if (this.temperature < 25) {return 'medium-usage'}
   else {return 'high-usage'}
